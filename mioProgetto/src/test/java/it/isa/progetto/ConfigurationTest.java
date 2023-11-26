2e84fda6816b216f6f21f34b99f0231c710f90dd
rachelerubini <rachele.rubini@edu.unife.it>
2023-11-26 19:04:27 +0100

    ConfigurationTest

---
+++ mioProgetto/src/test/java/it/isa/progetto/ConfigurationTest.java
@@ -0,0 +1,14 @@
+package it.isa.progetto;
+
+import org.junit.Test;
+
+public class ConfigurationTest 
+{
+
+    @Test
+    public void configurationTest()
+    {
+        new Configuration();
+    }
+    
+}
